3e46828571998c87948d436e0d71673a1fc9d748
Noah Wisch <noah.wisch@gmail.com>
2017-04-25 13:48:11 -0400

    deleted src and created new folder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// create new component
+// should produce some HTML
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// once created, must be rendered to DOM
